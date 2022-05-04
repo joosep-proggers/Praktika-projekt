80631e3c4b23c3750637279d4dcc96e256ae3b1c
joosep-proggers <joosep-elias.toru@khk.ee>
2022-05-04 22:58:17 +0300

    Functional front-end complete, needs polish

---
+++ filterType.js
@@ -0,0 +1,75 @@
+function populateOptions(array, select){
+    array.forEach(function(element,key){
+        select[key] = new Option(element, element)
+    })
+}
+
+document.addEventListener('change',(e) => {
+    if(e.target.id == "filterTypeModal" || e.target.id == "filterTypeNonModal"){
+        var parent = e.target.parentElement
+        while(parent.lastChild.id != "filterTypeModal" && parent.lastChild.id != "filterTypeNonModal"){
+            parent.removeChild(parent.lastChild)
+        }
+        var deleteRowButton = document.createElement("button")
+            deleteRowButton.textContent = "-"
+            deleteRowButton.className = "col-1"
+
+        if(e.target.value == "Amount"){
+            var moreLessEqual = document.createElement("select")
+            moreLessEqual.className ="col-2"
+            var mLEOptions = ["More", "Less", "Equal"]
+            populateOptions(mLEOptions, moreLessEqual)
+
+            var amount = document.createElement("input")
+            amount.type = "number"
+            amount.className = "col-2"
+
+            if(parent.lastChild.id == "filterTypeModal"){
+                amount.className = "col-3"
+                moreLessEqual.className = "col-3"
+            }
+
+            parent.appendChild(moreLessEqual)
+            parent.appendChild(amount)
+            parent.append(deleteRowButton)
+
+          } else if (e.target.value == "Title"){
+            var startsHasEnds = document.createElement("select")
+            startsHasEnds.className ="col-2"
+            var sHEOptions = ["Starts with", "Has", "Ends with"]
+            populateOptions(sHEOptions, startsHasEnds)
+
+            var text = document.createElement("input")
+            text.type = "text"
+            text.className = "col-2"
+
+            if(parent.lastChild.id == "filterTypeModal"){
+                text.className = "col-3"
+                startsHasEnds.className = "col-3"
+            }
+
+            parent.appendChild(startsHasEnds)
+            parent.appendChild(text)
+            parent.append(deleteRowButton)
+
+          } else if (e.target.value == "Date"){
+            var fromDuringBefore = document.createElement("select")
+            fromDuringBefore.className ="col-2"
+            var fDEOptions = ["From", "During", "Before"]
+            populateOptions(fDEOptions, fromDuringBefore)
+
+            var date = document.createElement("input")
+            date.type = "date"
+            date.className = "col-2"
+            
+            if(parent.lastChild.id == "filterTypeModal"){
+                date.className = "col-3"
+                fromDuringBefore.className = "col-3"
+            }
+
+            parent.appendChild(fromDuringBefore)
+            parent.appendChild(date)
+            parent.append(deleteRowButton)
+          }
+     }
+ });
--- restClient.js
+++ restClient.js
@@ -12,9 +12,21 @@ const vue = Vue.createApp({
         this.filters = await ()         saab api kaudu andmebaasist varasemad filtrid ja lisab menüüsse
     }*/
     methods: {
-        addFilter: function(){
+        addFilterModal: function(){
+            const addFilterNonModal = document.getElementById("addFilterNonModal")
+            if(addFilterNonModal.style != "display: none;"){
+                addFilterNonModal.style = "display: none;"
+            }
             const addFilterModal = new bootstrap.Modal(document.getElementById('addFilterModal'), {})
             addFilterModal.show()
+        },
+        addFilterNonModal: function(){
+            const addFilterNonModal = document.getElementById("addFilterNonModal")
+            addFilterNonModal.style = "display: block;"
+        },
+        closeNonModal: function(){
+            const addFilterNonModal = document.getElementById("addFilterNonModal")
+            addFilterNonModal.style = "display: none;"
         },
         addModalRow: function(){
             function populateOptions(array, select){
@@ -22,12 +34,12 @@ const vue = Vue.createApp({
                     select[key] = new Option(element, element)
                 })
             }
-            
-            const filterCriteria = document.getElementById('filterCriteria')
+            const filterCriteria = document.getElementById('filterCriteriaModal')
             var div = document.createElement("div")
             div.className = "row"
             var filter = document.createElement("select")
-            filter.className = "col-3 offset-2 filterType"
+            filter.className = "col-3 offset-2"
+            filter.id = "filterTypeModal"
             var options = ["Amount", "Title", "Date"]
             populateOptions(options, filter)
 
@@ -49,6 +61,61 @@ const vue = Vue.createApp({
             div.appendChild(amount)
             div.append(deleteRowButton)
             filterCriteria.appendChild(div)
+        }, 
+        addNonModalRow: function(){
+            function populateOptions(array, select){
+                array.forEach(function(element,key){
+                    select[key] = new Option(element, element)
+                })
+            }
+
+            const filterCriteria = document.getElementById('filterCriteriaNonModal')
+            var div = document.createElement("div")
+            div.className = "row"
+            var filter = document.createElement("select")
+            filter.className = "col-2 offset-3"
+            filter.id = "filterTypeNonModal"
+            var options = ["Amount", "Title", "Date"]
+            populateOptions(options, filter)
+
+            var moreLessEqual = document.createElement("select")
+            moreLessEqual.className ="col-2"
+            var mLEOptions = ["More", "Less", "Equal"]
+            populateOptions(mLEOptions, moreLessEqual)
+
+            var amount = document.createElement("input")
+            amount.type = "number"
+            amount.className = "col-2"
+
+            var deleteRowButton = document.createElement("button")
+            deleteRowButton.textContent = "-"
+            deleteRowButton.className = "col-1"
+
+            div.appendChild(filter)
+            div.appendChild(moreLessEqual)
+            div.appendChild(amount)
+            div.append(deleteRowButton)
+            filterCriteria.appendChild(div)
+        },
+        saveNonModal: function(){
+            const addFilterNonModal = document.getElementById("addFilterNonModal")
+            addFilterNonModal.style = "display: none;"
+            const filterCriteriaNonModal = document.getElementById("filterCriteriaNonModal")
+            let y = filterCriteriaNonModal.children.length
+            for (let i = 1; i < y; i++) {
+                console.log("removing child")
+                filterCriteriaNonModal.removeChild(filterCriteriaNonModal.lastChild)
+            }
+            alert("saved :D")
+        },
+        saveModal: function(){
+            const filterCriteriaModal = document.getElementById("filterCriteriaModal")
+            let y = filterCriteriaModal.children.length
+            for (let i = 1; i < y; i++) {
+                console.log("removing child")
+                filterCriteriaModal.removeChild(filterCriteriaModal.lastChild)
+            }
+            alert("saved :D")
         }
     }
 }).mount('body')
